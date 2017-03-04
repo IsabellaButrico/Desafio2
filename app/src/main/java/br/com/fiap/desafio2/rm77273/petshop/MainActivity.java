9498b3f2fe0c570d65639e3b1958b276cf5e7d15
Isabella Butrico <isabutrico0@gmail.com>
2017-03-04 15:07:30 -0300

    Arrumando erros

--- app/src/main/java/br/com/fiap/desafio2/rm77273/petshop/MainActivity.java
+++ app/src/main/java/br/com/fiap/desafio2/rm77273/petshop/MainActivity.java
@@ -6,15 +6,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
 
 
-    RadioGroup radioGroup;
+    RadioGroup grupo;
     CheckBox femea, adestrado, vacina;
-    TextView calculcar;
+    TextView calculo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,40 +26,36 @@ public class MainActivity extends AppCompatActivity {
 
 
 
- 	radioGroup = (RadioGroup)findViewById(R.id.rgGroup);
-    	txtView = (TextView)findViewById(R.id.calcular);
+        grupo= (RadioGroup)findViewById(R.id.rgGroup);
+    	calculo = (TextView)findViewById(R.id.calcular);
     	femea = (CheckBox)findViewById(R.id.femea);
     	adestrado = (CheckBox)findViewById(R.id.adestrado);
     	vacina = (CheckBox)findViewById(R.id.vacina);
-
-
-        Button btncalcular = (Button) findViewById(R.id.btncalcular);
-
 
     }
 
 
 
 
-     public void calcular(View v){
-        double valor = 0;
+     public void Calcular(View v){
+        double v = 0;
         double soma = 0;
-        int idRadio = radioGroup.getCheckedRadioButtonId();
+        int idRadio = grupo.getCheckedRadioButtonId();
         if(idRadio == R.id.radio_cachorro1){
-            valor = 800.00;
-            txtView.setText("R$ " + valor);
+            v = 800.00;
+            calculo.setText("R$ " + v);
         }else if(idRadio == R.id.cachorro2){
-            valor = 750.00;
-            txtView.setText("R$ " + valor);
+            v = 750.00;
+            calculo.setText("R$ " + v);
         }else if(idRadio == R.id.cachorro3){
-            valor = 700.00;
-            txtView.setText("R$ " + valor);
+            v = 700.00;
+            calculo.setText("R$ " + v);
         }else if(idRadio == R.id.cachorro4){
-            valor = 800.00;
-            txtView.setText("R$ " + valor);
+            v = 800.00;
+            calculo.setText("R$ " + v);
         }else{
-            valor = 00.00;
-            txtView.setText("R$ " + valor);
+            v = 00.00;
+            calculo.setText("R$ " + v);
         }
 
 
@@ -66,24 +63,24 @@ public class MainActivity extends AppCompatActivity {
 
  if(femea.isChecked()){
             soma = 180.00;
-            valor = (valor + soma);
-            txtView.setText("R$ " + valor);
+            v = (v + soma);
+     calculo.setText("R$ " + v);
         }
         if(adestrado.isChecked()){
             soma = 400.00;
-            valor = (valor + soma);
-            txtView.setText("R$ " + valor);
+            v = (v + soma);
+            calculo.setText("R$ " + v);
         }
         if(vacina.isChecked()){
             soma = 200.00;
-            valor = (valor + soma);
-            txtView.setText("R$ " + valor);
+            v = (v + soma);
+            calculo.setText("R$ " + v);
         }
 
         if(femea.isChecked() == false && adestrado.isChecked() == false && vacina.isChecked() == false){
             soma = 0;
-            valor = (valor + soma);
-            txtView.setText("R$ " + valor);
+            v = (v + soma);
+            calculo.setText("R$ " + v);
         }
 
     }
